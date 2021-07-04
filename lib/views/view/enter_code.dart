d8f267d3896f762b57a9b283dc85a38dfdc0daaa
iqfareez <iqmal3@outlook.com>
2021-07-04 18:44:44 +0800

    :art: Responsive layout

--- lib/views/view/enter_code.dart
+++ lib/views/view/enter_code.dart
@@ -8,24 +8,142 @@ import '../../utils/url_launcher.dart';
 import '../widgets/reuseable.dart';
 import 'user_card.dart';
 
-class EnterCode extends StatefulWidget {
-  EnterCode([this.userCode]);
+class EnterCode extends StatelessWidget {
+  const EnterCode([this.userCode]);
   final String userCode;
+
   @override
-  _EnterCodeState createState() => _EnterCodeState();
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: LayoutBuilder(
+          builder: (ctx, constraints) {
+            if (constraints.maxWidth < 460) {
+              return Stack(
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: VersionInfoText(),
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image.asset('images/logo/applogo.png', width: 100),
+                        InputCodeArea(
+                          userCode: userCode,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: MakeFlutreeButton(),
+                  ),
+                ],
+              );
+            } else {
+              return Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Align(
+                      alignment: Alignment.topRight, child: VersionInfoText()),
+                  Expanded(
+                    flex: 4,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: Padding(
+                            padding: const EdgeInsets.all(30.0),
+                            child: Image.asset(
+                              'images/logo/applogo.png',
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 3,
+                          child: InputCodeArea(
+                            userCode: userCode,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  MakeFlutreeButton(),
+                ],
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
 }
 
-class _EnterCodeState extends State<EnterCode> {
+class MakeFlutreeButton extends StatelessWidget {
+  const MakeFlutreeButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextButton(
+        onPressed: () => launchURL(context, kDynamicLink),
+        child: Text('Make your own Flutree profile!',
+            style: TextStyle(
+                fontSize: 15,
+                color: Colors.orange.shade700,
+                decorationStyle: TextDecorationStyle.dotted,
+                decoration: TextDecoration.underline)),
+      ),
+    );
+  }
+}
+
+class VersionInfoText extends StatelessWidget {
+  const VersionInfoText({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: FutureBuilder(
+        future: PackageInfo.fromPlatform(),
+        builder: (context, AsyncSnapshot<PackageInfo> snapshot) {
+          if (snapshot.hasData) {
+            return Text('V${snapshot.data.version}');
+          } else {
+            return SizedBox.shrink();
+          }
+        },
+      ),
+    );
+  }
+}
+
+class InputCodeArea extends StatefulWidget {
+  const InputCodeArea({Key key, this.userCode}) : super(key: key);
+  final userCode;
+
+  @override
+  _InputCodeAreaState createState() => _InputCodeAreaState();
+}
+
+class _InputCodeAreaState extends State<InputCodeArea> {
   final _usersCollection = FirebaseFirestore.instance.collection('users');
-
   final _formKey = GlobalKey<FormState>();
-
   final _codeController = TextEditingController();
-
   bool _isLoading = false;
-
   bool hasTryAccessProfile = false;
-
   String userCode;
 
   @override
@@ -78,91 +196,50 @@ class _EnterCodeState extends State<EnterCode> {
         accessProfile(userCode.trim());
       }
     });
-
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: Scaffold(
-        body: Stack(
-          children: [
-            Align(
-              alignment: Alignment.topRight,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: FutureBuilder(
-                  future: PackageInfo.fromPlatform(),
-                  builder: (context, AsyncSnapshot<PackageInfo> snapshot) {
-                    if (snapshot.hasData) {
-                      return Text('V${snapshot.data.version}');
-                    } else {
-                      return SizedBox.shrink();
-                    }
-                  },
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text('Enter Flutree code', style: TextStyle(fontSize: 26)),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 60.0),
+          child: Form(
+            key: _formKey,
+            child: Container(
+              width: 360,
+              child: TextFormField(
+                textAlign: TextAlign.center,
+                validator: (value) =>
+                    value.length < 5 ? 'Not enough character' : null,
+                controller: _codeController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(32),
+                  ),
                 ),
+                textInputAction: TextInputAction.done,
+                keyboardType: TextInputType.text,
               ),
             ),
-            Align(
-              alignment: Alignment.center,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset('images/logo/applogo.png', width: 100),
-                  Text('Enter Flutree code', style: TextStyle(fontSize: 28)),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 24.0, horizontal: 60.0),
-                    child: Form(
-                      key: _formKey,
-                      child: TextFormField(
-                        textAlign: TextAlign.center,
-                        validator: (value) =>
-                            value.length < 5 ? 'Not enough character' : null,
-                        controller: _codeController,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(32),
-                          ),
-                        ),
-                        textInputAction: TextInputAction.done,
-                        keyboardType: TextInputType.text,
-                      ),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: _isLoading
-                        ? null
-                        : () async {
-                            FocusScope.of(context).unfocus();
-                            if (_formKey.currentState.validate()) {
-                              accessProfile(_codeController.text.trim());
-                            }
-                          },
-                    child: !_isLoading ? Text('Go') : LoadingIndicator(),
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextButton(
-                    onPressed: () => launchURL(context, kDynamicLink),
-                    child: Text('Make your own Flutree profile!',
-                        style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.orange.shade700,
-                            decorationStyle: TextDecorationStyle.dotted,
-                            decoration: TextDecoration.underline)),
-                  ),
-                )),
-          ],
+          ),
         ),
-      ),
+        ElevatedButton(
+          onPressed: _isLoading
+              ? null
+              : () async {
+                  FocusScope.of(context).unfocus();
+                  if (_formKey.currentState.validate()) {
+                    accessProfile(_codeController.text.trim());
+                  }
+                },
+          child: !_isLoading ? Text('Go') : LoadingIndicator(),
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 18),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
@@ -190,7 +267,7 @@ class NotFoundDialog extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Text(
                 'User not found. Please try again or check the link if entered correctly.'),
-          )
+          ),
         ],
       ),
     );
